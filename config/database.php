067e8f2a465bbf4b5d446e0d14327d33623392f6
JatoPriestlyNjilah <priestlynjilah@gmail.com>
2024-07-03 12:34:07 +0100

    started editing php file

--- config/database.php
+++ config/database.php
@@ -63,6 +63,21 @@ return [
             ]) : [],
         ],
 
+        'BackEnd-API' => [
+        'driver' => 'mysql',
+        'host' => env('DB_HOST', '127.0.0.1'),
+        'port' => env('DB_PORT', '3326'),
+        'database' => env('DB_DATABASE', 'my_database'),
+        'username' => env('DB_USERNAME', 'my_username'),
+        'password' => env('DB_PASSWORD', 'my_password'),
+        'unix_socket' => env('DB_SOCKET', ''),
+        'charset' => 'utf8mb4',
+        'collation' => 'utf8mb4_unicode_ci',
+        'prefix' => '',
+        'strict' => true,
+        'engine' => null,
+    ],        
+
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
